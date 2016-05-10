640ab8079b8249f8ae194e34665c57c8acc3ede9
I Putu Agus Suarya W <agus.suarya@yahoo.co.id>
2016-05-11 00:44:52 +0800

    cetak biru
    + desain bagus & user friendly

---
+++ AIC Scanner2/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace AIC_Scanner2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.pengaturanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.setLokasiSimpanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(33, 85);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(459, 47);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Scan";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 226);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(633, 128);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Daftar berkas";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(81, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(546, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Direktori aktif";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pengaturanToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(657, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // pengaturanToolStripMenuItem
+            // 
+            this.pengaturanToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.setLokasiSimpanToolStripMenuItem});
+            this.pengaturanToolStripMenuItem.Name = "pengaturanToolStripMenuItem";
+            this.pengaturanToolStripMenuItem.Size = new System.Drawing.Size(80, 20);
+            this.pengaturanToolStripMenuItem.Text = "Pengaturan";
+            // 
+            // setLokasiSimpanToolStripMenuItem
+            // 
+            this.setLokasiSimpanToolStripMenuItem.Name = "setLokasiSimpanToolStripMenuItem";
+            this.setLokasiSimpanToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
+            this.setLokasiSimpanToolStripMenuItem.Text = "Set lokasi simpan";
+            this.setLokasiSimpanToolStripMenuItem.Click += new System.EventHandler(this.setLokasiSimpanToolStripMenuItem_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(33, 49);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(459, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Nama direktori :";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Location = new System.Drawing.Point(65, 43);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(528, 168);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 380);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Proses";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(60, 375);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(579, 23);
+            this.progressBar1.TabIndex = 9;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column2,
+            this.Column3,
+            this.Column1});
+            this.dataGridView1.Location = new System.Drawing.Point(6, 61);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(621, 61);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // Column2
+            // 
+            this.Column2.FillWeight = 30.45685F;
+            this.Column2.HeaderText = "No";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.FillWeight = 134.7716F;
+            this.Column3.HeaderText = "Nama file";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column1
+            // 
+            this.Column1.FillWeight = 134.7716F;
+            this.Column1.HeaderText = "Lokasi";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 410);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "AIC Scanner2";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem pengaturanToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem setLokasiSimpanToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+    }
+}
+
---
+++ AIC Scanner2/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AIC_Scanner2
+{
+    public partial class Form1 : Form
+    {
+        String lokasi_simpan, nama_folder_penyimpanan_hasil_scanner;
+        String[] daftar_file_direktori_aktif, list_nama_aktif;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            lokasi_simpan = "";
+        }
+
+        private void pilih_lokasi_simpan()
+        {
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+
+            if(lokasi_simpan=="")
+            {
+                folderDlg.ShowNewFolderButton = true;
+                // Show the FolderBrowserDialog.
+                DialogResult result = folderDlg.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    lokasi_simpan = folderDlg.SelectedPath;
+
+                    MessageBox.Show("Berhasil !\nLokasi simpan hasil pemindai pada direktori : " + lokasi_simpan, "Berhasil", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+
+
+            else
+            {
+                if (MessageBox.Show("Lokasi simpan sudah di set pada direktori "+lokasi_simpan+"\nApakah anda ingin menggantinya ?", "Peringatan", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+                {
+                    folderDlg.ShowNewFolderButton = true;
+                    // Show the FolderBrowserDialog.
+                    DialogResult result = folderDlg.ShowDialog();
+                    if (result == DialogResult.OK)
+                    {
+                        lokasi_simpan = folderDlg.SelectedPath;
+                        MessageBox.Show("Berhasil !\nLokasi simpan hasil pemindai pada direktori : " + lokasi_simpan, "Berhasil", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            } 
+        }
+
+        private void setLokasiSimpanToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pilih_lokasi_simpan();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(lokasi_simpan=="")
+            {
+                MessageBox.Show("Lokasi simpan belum ditentukan !.\nSilakan tentukan lokasi simpan pada :\nPengaturan -> Set lokasi simpan", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if(textBox2.Text.Trim()=="")
+            {
+                MessageBox.Show("Nama direktori untuk hasil pemindaian belum di tentukan !.\nSilakan ketik nama direktorinya terlebih dahulu.", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                if (MessageBox.Show("Hasil pemindaian akan disimpan pada folder \"" + textBox2.Text.Trim() + "\".\nApakah anda yakin ? ", "Peringatan", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+                {
+                    nama_folder_penyimpanan_hasil_scanner = textBox2.Text.Trim();
+                    textBox1.Text = lokasi_simpan + "\\" + nama_folder_penyimpanan_hasil_scanner;
+                    if (!Directory.Exists(textBox1.Text))
+                        Directory.CreateDirectory(textBox1.Text);
+                    daftar_file_direktori_aktif = Directory.GetFiles(@"" + textBox1.Text);
+                    tampilkan_isi();
+
+                   
+                    //scan...
+                }
+            }           
+        }
+
+        private void tampilkan_isi()
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Refresh();
+
+            buat_nama_file();
+
+            for (int i = 0; i < daftar_file_direktori_aktif.Length; i++)
+            {
+                dataGridView1.Rows.Add(1);
+                dataGridView1.Rows[i].Cells[0].Value = (i + 1).ToString();
+                dataGridView1.Rows[i].Cells[1].Value = list_nama_aktif[i];
+                dataGridView1.Rows[i].Cells[2].Value = daftar_file_direktori_aktif[i];
+            }
+        }
+
+        private void buat_nama_file()
+        {
+            int start_indek = lokasi_simpan.Length + nama_folder_penyimpanan_hasil_scanner.Length + 2;
+            list_nama_aktif = new string[daftar_file_direktori_aktif.Length];
+
+            for (int i = 0; i < daftar_file_direktori_aktif.Length; i++)
+            {
+                list_nama_aktif[i] = daftar_file_direktori_aktif[i].Substring(start_indek);
+            }
+        }
+    }
+}
